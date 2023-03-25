b061ec6999cd755156d7a618c880688f46ea18af
TamerJr <ahmttmer@gmail.com>
2023-03-25 23:05:56 +0200

    Finishing The structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import {Account ,Home ,Shop ,ProductDetails ,LogIn ,SignUp,Terms ,AboutUs} from "./Utilities/Utilities"
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-     
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/account"  element={<Account/>}/>
+        <Route path="/shop" element={<Shop/>}/>
+        <Route path="/shop/product/:id" element={<ProductDetails/>} />
+        <Route path="login"  element={<LogIn/>}/>
+        <Route path="signup"  element={<SignUp/>}/>
+        <Route path="/terms"  element={<Terms/>}/>
+        <Route path="/aboutus"  element={<AboutUs/>}/>
+
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
