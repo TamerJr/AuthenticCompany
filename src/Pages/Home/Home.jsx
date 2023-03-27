0a901c7bfd5ba295fb0567b442a80fac0b4bc0a5
TamerJr <ahmttmer@gmail.com>
2023-03-27 03:01:14 +0200

    HomePage Components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,33 @@
-import React, { useContext } from 'react'
-import userContext from '../../Contexts/AuthContext'
-
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import userContext from "../../Contexts/AuthContext";
+import {
+  ClientCounter,
+  ProductCard,
+  ValueSection,
+} from "../../Utilities/Utilities";
 const Home = () => {
-  const {user}=useContext(userContext)
-  console.log(user)
+  const { user } = useContext(userContext);
+  console.log(user);
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <ValueSection />
+      <ProductCard />
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet vel
+          veritatis cupiditate rem neque reprehenderit natus ratione aut,
+          necessitatibus numquam nihil sunt amet sapiente omnis repudiandae
+          dolores magnam pariatur voluptatum laboriosam quis. Cum quasi itaque
+          ipsum fuga iure ipsa alias?
+        </p>
+        <button>
+          <Link to="aboutus">More</Link>
+        </button>
+      </section>
+      <ClientCounter />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
