33117f3521cc6b9513e16b4ec3554756c95a99e5
TamerJr <ahmttmer@gmail.com>
2023-03-30 16:32:17 +0200

    Terms Page Done

---
+++ src/TermsData.js
@@ -0,0 +1,19 @@
+const TermsData=[
+    {
+        "name":"Cookies",
+        "disc":"We employ the use of cookies. By accessing Coaching Company, you agreed to use cookies in agreement with the Authentic's Privacy Policy.Most interactive websites use cookies to let us retrieve the userâ€™s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies."
+    }  , {
+        "name":"License",
+        "disc":"Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Authentic does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Authentic,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, Authentic shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website."
+    } ,  {
+        "name":"iFrames",
+        "disc":"Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website."
+    }  , {
+        "name":"Content Liability",
+        "disc":"We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights."
+    }  , {
+        "name":"Reservation of Rights",
+        "disc":"We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and itâ€™s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions."
+    }
+]
+export default TermsData
