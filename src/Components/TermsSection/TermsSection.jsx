044ddc80fd2e349f23152a3bedbc16595fba7db4
TamerJr <ahmttmer@gmail.com>
2023-03-30 16:31:19 +0200

    DoneTerms Page

--- src/Components/TermsSection/TermsSection.jsx
+++ src/Components/TermsSection/TermsSection.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
-const TermsSection = () => {
+import React, { useEffect, useRef, useState } from "react";
+import "./TermsSection.css";
+import { TbDirectionSignFilled } from "react-icons/tb";
+import { refEqual } from "firebase/firestore";
+const TermsSection = ({ name, disc}) => {
+  const [toggleSection, setToggleSetion] = useState(false);
+  const handleSection = () => {
+    setToggleSetion(!toggleSection);
+  };
+  
   return (
-    <div>TermsSection</div>
-  )
-}
+    <section className="TermsSection">
+      <span className="SectionController" onClick={handleSection}>
+        <h3>{name}</h3>
+        <TbDirectionSignFilled
+          size={25}
+          className={toggleSection ? "SectionTogglerDown" : "SectionTogglerUp"}
+        />
+      </span>
+      <p
+        className={
+          toggleSection 
+            ? "TermsSectionText TermsSectionDiscriptionOn"
+            : "TermsSection TermsSectionDiscriptionOff"
+        }
+      >
+        {disc}
+      </p>
+    </section>
+  );
+};
 
-export default TermsSection
+export default TermsSection;
--- src/Pages/Terms/Terms.jsx
+++ src/Pages/Terms/Terms.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React, { useEffect, useRef, useState } from "react";
+import { TermsSection } from "../../Utilities/Utilities";
+import TermsData from "../../TermsData";
 const Terms = () => {
-  return (
-    <div>Terms</div>
-  )
-}
 
-export default Terms
+  return (
+    <div>
+      {TermsData?.map((ele) => (
+        <TermsSection
+          {...ele}
+
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Terms;
